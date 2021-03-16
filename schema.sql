e4ea771d81ad0d129ecb1cc344b3e0d47cd4ae05
Kevin Ng <kevin.the.engi@gmail.com>
2021-03-16 10:02:54 -0700

    sql schema change

--- schema.sql
+++ schema.sql
@@ -4,15 +4,24 @@ CREATE DATABASE Products;
 
 USE Products;
 
+CREATE TABLE ProductList (
+  ID
+    INT AUTO_INCREMENT,
+  productID
+    INT,
+  PRIMARY KEY(ID),
+  FOREIGN KEY(ID) REFERENCES ProductInfo(productID)
+)
+
 CREATE TABLE ProductInfo (
   productID
     INT AUTO_INCREMENT,
   name
     VARCHAR(20) NOT NULL DEFAULT '',
   slogan
-    VARCHAR(50) NOT NULL DEFAULT '',
-  description
     VARCHAR(100) NOT NULL DEFAULT '',
+  description
+    VARCHAR(255) NOT NULL DEFAULT '',
   category
     CHAR(20) NOT NULL DEFAULT '',
   PRIMARY KEY(productID)
@@ -24,7 +33,9 @@ CREATE TABLE ProductFeatures (
   feature
     VARCHAR(20) NULL,
   feature_value
-    VARCHAR(20) NULL
+    VARCHAR(20) NULL,
+  productID
+    INT,
   PRIMARY KEY(featureID),
   FOREIGN KEY(featureID) REFERENCES ProductInfo(productID)
 );
@@ -40,7 +51,7 @@ CREATE TABLE ProductStyles (
     DECIMAL NOT NULL DEFAULT '99999.99',
   default
     BOOLEAN NOT NULL DEFAULT 'false',
-  product_id
+  productID
     INT,
   PRIMARY KEY(styleID),
   FOREIGN KEY(styleID) REFERENCES ProductInfo(productID)
@@ -50,9 +61,9 @@ CREATE TABLE ProductPhotos (
   photoID
     MEDIUMINT AUTO_INCREMENT,
   thumbnail_url
-    VARCHAR(200) NULL,
+    VARCHAR(255) NULL,
   url
-    VARCHAR(200) NULL,
+    VARCHAR(255) NULL,
   styleID
     INT,
   PRIMARY KEY(photoID),
